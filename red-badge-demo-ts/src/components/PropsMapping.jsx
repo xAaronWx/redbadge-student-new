cc2f473cfdd05b42f9c6ecf6922371550b8e0c00
xAaronWx <50504602+xAaronWx@users.noreply.github.com>
2021-03-23 19:26:53 -0400

    New Branch

---
+++ red-badge-demo-ts/src/components/PropsMapping.jsx
@@ -0,0 +1,16 @@
+import React, { Components } from "react";
+
+class PropsMappingExample extends React.Component {
+  render() {
+    const visitedPlaces = () =>
+      this.props.visited.map((place) => <li>{place}</li>);
+    return (
+      <div>
+        <h2>Places I have been</h2>
+        <ul>{visitedPlaces()}</ul>
+      </div>
+    );
+  }
+}
+
+export default PropsMappingExample;
