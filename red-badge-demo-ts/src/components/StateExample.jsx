bd7c088bf59ac99401177f67324d5d6e04b85e69
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-03-25 18:28:41 -0400

    update

--- red-badge-demo-ts/src/components/StateExample.jsx
+++ red-badge-demo-ts/src/components/StateExample.jsx
@@ -4,17 +4,21 @@ class StateExample extends Component {
 
   constructor(props){
     super(props);
-    this.state = {currentTemp: '78 Degrees'}
-    this.handleClick = this.handleClick.bind(this);
+    this.state = {currentTemp: '78 Degrees',  location: 'Indiana' }
+    // this.handleClick = this.handleClick.bind(this);
   }
   
-  handleClick(){
-    this.setState ({currentTemp: '48 Degrees'});
-  }
+  // handleClick(){
+  //   this.setState ({currentTemp: '48 Degrees'});
+  // }
+
+  handleClick = ()=> {
+    this.setState({currentTemp: '28'});
+  };
 
   render() {
     return (
-    <div>Hello the current temp is {this.state.currentTemp}
+    <div>Hello the current temp is {this.state.currentTemp} - in {this.state.location}
       <button onClick={this.handleClick} >Click m to change temp</button>
     </div>);
   }
