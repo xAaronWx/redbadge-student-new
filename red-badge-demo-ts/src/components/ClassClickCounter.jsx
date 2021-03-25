4fe2299524d175db680779fee0dfa0743acc4ec0
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-03-25 19:27:12 -0400

    change

--- red-badge-demo-ts/src/components/ClassClickCounter.jsx
+++ red-badge-demo-ts/src/components/ClassClickCounter.jsx
@@ -3,21 +3,21 @@ import { Button, Container } from "reactstrap";
 
 class ClassClickCounter extends Component {
 
-   constructor(props) {
+  constructor(props) {
     super(props);
-    this.state = {countValue: this.props.initialValue};
+    this.state = { countValue: this.props.initialValue };
   }
 
   handleClick = () => {
-    this.setState({ countValue: this.state.countValue -1 });
+    this.setState({ countValue: this.state.countValue - 1 });
   }
 
   render() {
     return (
       <Container>
-       <p>The total clicks left are {this.state.countValue}</p>
-       <Button onClick={this.handleClick}> Click Me From Function</Button>
-    </Container>);
+        <p>The total clicks left are {this.state.countValue}</p>
+        <Button onClick={this.handleClick}> Click Me From Function</Button>
+      </Container>);
   }
 }
 export default ClassClickCounter;
